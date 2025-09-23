adf7fb2f676ef60c1f939e206990ffea45c940e1
Marcy Rattner <mrattner42@outlook.com>
2025-09-23 16:53:55 -0700

    style: run ruff formatter

--- {{ cookiecutter.__project_slug }}/{{ cookiecutter.module_name }}/create_db.py
+++ {{ cookiecutter.__project_slug }}/{{ cookiecutter.module_name }}/create_db.py
@@ -1,17 +1,18 @@
-"""
-Initializes the database for the given environment with the {{ cookiecutter.project_name }} schema.
-"""
+"""Initializes the database for the given environment with the {{ cookiecutter.project_name }} schema."""
+
 import typer
 
-from {{ cookiecutter.module_name }}.models import Base
 from {{ cookiecutter.module_name }}.database import Database as Db
+from {{ cookiecutter.module_name }}.models import Base
 
 
 def main(environment: str = "DEFAULT"):
-"""Re-runs the ORM schema creation on the database in the given environment."""
-    confirm = typer.confirm("Are you sure you want to drop and re-create the database schema?")
+    """Re-run the ORM schema creation on the database in the given environment."""
+    confirm = typer.confirm(
+        "Are you sure you want to drop and re-create the database schema?"
+    )
     if not confirm:
-        raise typer.Abort()
+        raise typer.Abort
     Db.init(environment, echo=True)
     Base.metadata.create_all(Db.engine)
     typer.echo("Database is now ready to use.")
@@ -19,4 +20,3 @@ def main(environment: str = "DEFAULT"):
 
 if __name__ == "__main__":
     typer.run(main)
-
