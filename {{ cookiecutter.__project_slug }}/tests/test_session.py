97328fa23441dfe35676c27ab0780649cc76f8f5
Marcy Rattner <mrattner42@outlook.com>
2025-10-01 11:14:38 -0700

    test: Flesh out example database integration tests
    Update to SQLAlchemy 2.0's connection and transaction logic
    which replaces the old-style Session. A custom context manager
    wraps the connection so that its transaction is rolled back
    at the end rather than committed (the default behavior for
    connection.begin() context manager).

--- {{ cookiecutter.__project_slug }}/tests/test_session.py
+++ {{ cookiecutter.__project_slug }}/tests/test_session.py
@@ -1,22 +1,66 @@
+import io
 import unittest
+from contextlib import contextmanager
 
-from {{ cookiecutter.module_name }}.database import Database as Db
+from sqlalchemy import func, insert, select
+
+import {{ cookiecutter.module_name }}.models as m
+from {{ cookiecutter.module_name }}.database import get_db
 
 
-class TestSession(unittest.TestCase):
-    """Base class for a database integration test."""
+class DbIntegrationTest(unittest.TestCase):
+    """Initializes in-memory SQLite engine and wraps each test in a connection with a transaction."""
+
+    @classmethod
+    def setUpClass(cls):
+        cls.engine = get_db(io.StringIO(""))
+        m.init_schema(cls.engine)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.engine.dispose()
+
+    @contextmanager
+    def isolated_txn(self):
+        conn = self.engine.connect()
+        try:
+            yield conn
+        finally:
+            conn.rollback()
 
     def setUp(self):
-        """Connect to the test database and begin a wrapper transaction."""
-        Db.init(environment="Test")
-        self.connection = Db.engine.connect()
-        self.transaction = self.connection.begin()
-
-    def tearDown(self):
-        """Rollback the wrapper transaction and close the connection."""
-        self.transaction.rollback()
-        self.connection.close()
+        self.conn = self.enterContext(self.isolated_txn())
 
 
-class ExampleTest(TestSession):
-    pass
+class TestFoo(DbIntegrationTest):
+    def test_insert_org(self):
+        insert_stmt = insert(m.organization).values(name="FlortCo", prefix="3f0d")
+        self.conn.execute(insert_stmt)
+
+        count_stmt = select(func.count()).select_from(m.organization)
+        count = self.conn.scalar(count_stmt)
+        self.assertEqual(count, 1)
+
+        query = select(m.organization).where(m.organization.c.name == "FlortCo")
+        result = self.conn.execute(query)
+        row = result.one()
+        self.assertEqual(row.prefix, "3f0d")
+
+    def test_insert_users(self):
+        insert_stmt = insert(m.user).values(
+            [
+                {"name": "SpongeBob Squarepants", "email": "spongebob@flort.co"},
+                {"name": "Sandy Cheeks", "email": "sandy@flort.co"},
+                {"name": "Patrick Star", "email": "patrick@flort.co"},
+            ]
+        )
+        self.conn.execute(insert_stmt)
+
+        count_stmt = select(func.count()).select_from(m.user)
+        count = self.conn.scalar(count_stmt)
+        self.assertEqual(count, 3)
+
+        query = select(m.user).where(m.user.c.email == "patrick@flort.co")
+        result = self.conn.execute(query)
+        row = result.one()
+        self.assertEqual(row.name, "Patrick Star")
